7ae3889e26ed50e47c8558e88be27d6ea581f2e9
emersonml <artes.emerson@gmail.com>
2022-06-10 16:04:51 -0300

    reorganizando pastas styles

--- app/controllers/webcoruja/welcome_controller.rb
+++ app/controllers/webcoruja/welcome_controller.rb
@@ -9,13 +9,26 @@ class Webcoruja::WelcomeController < WebcorujaController
   #  puts = "Rails.root.join('node_modules')"
   #  puts = Rails.root.join('node_modules')
      
-
+ 
   #  infotxt = %x(cat /home/emerson/projects/sys5gbrasil/lib/mtik/info.txt)
   #  infotxt = %x(cat /home/emerson/projects/mtik/info.mk.txt)
    infotxt = %x(cat /home/emerson/projects/mtik/info.mk.txt)
    @infomk = infotxt.split(" ")
 
- end
+  #  redirect_back_or_to({ action: "index", id: 5 })
+  # redirect_to action: "reload"
+  # reload()
+
+
+end
+
+
+def reload
+  
+  # sleep 10
+  #  redirect_to root_path
+  #  redirect_back({ fallback_location: 'root_path' })
+end
 
 
 
