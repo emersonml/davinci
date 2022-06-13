b180ead4d314a9f078c8ef922f9e47a77e15e0e0
emersonml <artes.emerson@gmail.com>
2022-06-13 12:59:08 -0300

    setInterval com xmlHttpRequest

--- app/assets/javascripts/webcoruja/welcome.js
+++ app/assets/javascripts/webcoruja/welcome.js
@@ -1,63 +1,76 @@
+////////////////////////////////  option 1
 
+// window.setInterval('aaa()', 30000)
 
+// function aaa() {
 
+// $.ajax({
+//   url: 'http://5gbrasil.net.br:3003/webcoruja/welcome/dados',
+//   // uri: 'webcoruja/welcome/dados',
+//   method: 'GET',
+//   // data: dados,
+//   processData: false,
+//   contentType: false
 
+// }).done(function(result){
+// // window.alert(resposta);
+// $('#cardActives').text(result)
+// })
 
-// var xmlHttp;
-// if(window.XMLHttpRequest){
-//   xmlHttp = new XMLHttpRequest;
-// }else{
-//   xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
 // }
+
+////////////////////////////////  option 1
+
+
+
+////////////////////////////////  option 2
+var xmlHttp;
+if(window.XMLHttpRequest){
+  xmlHttp = new XMLHttpRequest;
+}else{
+  xmlHttp = new ActiveXObject("Microsoft.XMLHTTP");
+}
 
 // xmlHttp.onreadystatechange = function(){
-//   if(xmlHttp.readyState == 4 && xmlHttp.status == 200){
-//     console.log(xmlHttp.responseText);
-//   }
-// }
+  //   if(xmlHttp.readyState == 4 && xmlHttp.status == 200){
+    //     console.log(xmlHttp.responseText);
+    //   }
+    // }
+    window.setInterval('bbb()', 30000)
+    
+    function bbb() {
+      xmlHttp.open('GET', 'http://5gbrasil.net.br:3003/webcoruja/welcome/dados', false);
+      // xmlHttp.open('GET', 'dados', true);
+      xmlHttp.send();
+      
+      
+      // $("#cardActives").text(responseText)
+      document.getElementById('cardActives').innerHTML = xmlHttp.responseText
+    }
+    ////////////////////////////////  option 2
+    
 
-// xmlHttp.open('GET', 'http://5gbrasil.net.br:3003/webcoruja/welcome/dados', true);
-// // xmlHttp.open('GET', 'dados', true);
-// // xmlHttp.send(Document.getElementById('cardActives'));
-
-
-// // $("#cardActives").text(responseText)
-
-
-
-var counter = 0
-
-window.setInterval('aaa()', 30000)
-
-function refreshDiv() {
-  counter = counter + 1
-  document.getElementById('cardActives').innerHTML = xmlHttp.send() + counter
-}
+    
 
 
-//////////////////////////////////////////////////////
-// var dados = new FormData();
-// dados.append('nome', 'Emerson');
-// dados.append('idade', 37);
-// dados.append('end', 'Rua Lobato');
 
 
-function aaa() {
 
-$.ajax({
-  url: 'http://5gbrasil.net.br:3003/webcoruja/welcome/dados',
-  // uri: 'webcoruja/welcome/dados',
-  method: 'GET',
-  // data: dados,
-  processData: false,
-  contentType: false
+    ////////////////////////////////  testes
+    // var counter = 0
+    // function refreshDiv() {
+      //   counter = counter + 1
+      //   document.getElementById('cardActives').innerHTML = xmlHttp.send() + counter
+      // }
 
-}).done(function(result){
-// window.alert(resposta);
-$('#cardActives').text(result)
-})
 
-}
+      //////////////////////////////////////////////////////
+      // var dados = new FormData();
+      // dados.append('nome', 'Emerson');
+      // dados.append('idade', 37);
+      // dados.append('end', 'Rua Lobato');
+
+
 
 // $.ajax({
 //   url: "/api/getWeather",
